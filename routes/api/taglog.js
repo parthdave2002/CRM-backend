b67c003888675b1c1f56c2c3fa4ff5db78c0b941
Parth Dave <daveparth2604@gmail.com>
2025-04-19 01:26:10 +0530

    taglog bug resolved

--- routes/api/taglog.js
+++ routes/api/taglog.js
@@ -4,17 +4,17 @@ const uploadHelper = require("../../helper/upload.helper")
 const TaglogController = require('../../modules/taglog/taglogController');
 const { authentication, authorization } = require('../../middleware/auth.middleware');
 
-router.get('/get-taglog',authentication,authorization("Taglog"), TaglogController.getAllTaglogList);
-router.post('/add-taglog',authentication,authorization("Taglog"), TaglogController.AddTaglog);
-router.delete('/remove-taglog',authentication,authorization("Taglog"), TaglogController.DeleteTaglog);
-router.delete('/status-taglog',authentication,authorization("Taglog"), TaglogController.changestatus);
+router.get('/get-taglog',authentication, TaglogController.getAllTaglogList);
+router.post('/add-taglog',authentication, TaglogController.AddTaglog);
+router.delete('/remove-taglog',authentication, TaglogController.DeleteTaglog);
+router.delete('/status-taglog',authentication, TaglogController.changestatus);
 
-router.get('/get-subtaglog', authentication, authorization("Taglog"), TaglogController.getAllSubtaglog);
-router.post('/add-subtaglog', authentication, authorization("Taglog"), TaglogController.AddSubtaglog);
-router.delete('/remove-subtaglog', authentication, authorization("Taglog"), TaglogController.DeleteSubtaglog);
-router.delete('/update-subtaglog', authentication, authorization("Taglog"), TaglogController.updateSubtaglog);
+router.get('/get-subtaglog', authentication,  TaglogController.getAllSubtaglog);
+router.post('/add-subtaglog', authentication,  TaglogController.AddSubtaglog);
+router.delete('/remove-subtaglog', authentication,  TaglogController.DeleteSubtaglog);
+router.delete('/update-subtaglog', authentication,  TaglogController.updateSubtaglog);
 
-router.post('/add-customer-taglog', authentication, authorization("Taglog"), TaglogController.AddTaglogCustomer);
-router.get('/get-customer-taglogs', authentication, authorization("Taglog"), TaglogController.getAllTaglogCustomers);
+router.post('/add-customer-taglog', authentication, TaglogController.AddTaglogCustomer);
+router.get('/get-customer-taglog', authentication,  TaglogController.getAllTaglogCustomers);
 
 module.exports = router
